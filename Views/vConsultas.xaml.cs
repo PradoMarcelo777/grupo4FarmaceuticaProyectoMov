1b787f3682e75ea542d4695be57756daf271b544
PradoMarcelo777 <geotzmprado777@outlook.com>
2024-11-20 15:17:33 -0500

    Creacion vista de Consulta
    Creacion vista consulta y metodos de los btn

---
+++ Views/vConsultas.xaml.cs
@@ -0,0 +1,24 @@
+namespace grupo4FarmaceuticaProyectoMov.Views;
+
+public partial class vConsultas : ContentPage
+{
+	public vConsultas()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnGuardar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnSalir_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
