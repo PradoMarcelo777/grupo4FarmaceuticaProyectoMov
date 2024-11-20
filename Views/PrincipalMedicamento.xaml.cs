f4b8cccee4a9fd8949e35d9e10c172bd0a047eee
Deinadayana <157863402+Deinadayana@users.noreply.github.com>
2024-11-20 16:55:32 -0500

    PrincipalMedicamento
    Creación de la vista

---
+++ Views/PrincipalMedicamento.xaml.cs
@@ -0,0 +1,14 @@
+namespace grupo4FarmaceuticaProyectoMov.Views;
+
+public partial class PrincipalMedicamento : ContentView
+{
+	public PrincipalMedicamento()
+	{
+		InitializeComponent();
+	}
+
+    private void btnInsertar_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
